33cc4216ccac52b97f3c7f73bfc63808ac78e7b2
Văn Trung <137430777+vantrung1109@users.noreply.github.com>
2024-05-30 22:11:55 +0700

    Revert "ok"
    This reverts commit 43300c6d95a38e3835aa370a981e7e4ce61edea9.

--- app/src/main/java/com/example/projectfinaltth/ui/instructor/InstructorLessonActivity.java
+++ app/src/main/java/com/example/projectfinaltth/ui/instructor/InstructorLessonActivity.java
@@ -2,25 +2,21 @@ package com.example.projectfinaltth.ui.instructor;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
-import android.widget.RatingBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.projectfinaltth.R;
 import com.example.projectfinaltth.data.ApiService;
 import com.example.projectfinaltth.data.ShareRefences.DataLocalManager;
 import com.example.projectfinaltth.data.model.request.CourseIdRequest;
-import com.example.projectfinaltth.data.model.response.courseIntro.CourseIntroResponse;
+import com.example.projectfinaltth.data.model.response.course.CourseItem;
 import com.example.projectfinaltth.data.model.response.lesson.LessonItem;
 import com.example.projectfinaltth.ui.adapter.InstructorLessonAdapter;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,31 +32,19 @@ public class InstructorLessonActivity extends AppCompatActivity {
     private List<LessonItem> lessonItemList;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
-    private TextView tvCourseName, tvCourseAuthor, tvDescriptionContent, tvCoursePrice;
-    private RatingBar ratingBar;
-    private ImageView imgCourseIntro;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_lesson);
 
-        // Initialize views
         lessonsRecyclerView = findViewById(R.id.recyclerView);
-        tvCourseName = findViewById(R.id.tv_course_name);
-        tvCourseAuthor = findViewById(R.id.tv_course_author);
-        tvDescriptionContent = findViewById(R.id.tv_description_content);
-        tvCoursePrice = findViewById(R.id.tv_course_price);
-        //ratingBar = findViewById(R.id.tv_rating);
-        imgCourseIntro = findViewById(R.id.img_course_intro);
-
         lessonsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         lessonItemList = new ArrayList<>();
         lessonAdapter = new InstructorLessonAdapter(this, lessonItemList, new InstructorLessonAdapter.OnItemInteractionListener() {
             @Override
             public void onDeleteLesson(int position, LessonItem lessonItem) {
-                deleteLessonItem(position, lessonItem);
+                deleteLessonItem(position,lessonItem);
             }
         });
 
@@ -74,20 +58,6 @@ public class InstructorLessonActivity extends AppCompatActivity {
         } else {
             Log.e("InstructorLesson", "Course ID is null or empty");
         }
-        CourseIntroResponse courseIntroResponse = (CourseIntroResponse) getIntent().getSerializableExtra("courseIntro");
-        if (courseIntroResponse == null) {
-            Toast.makeText(this, "No course data", Toast.LENGTH_SHORT).show();
-            finish();
-            return;
-        }
-
-        // Populate course details
-        tvCourseName.setText(courseIntroResponse.getCourse().getTitle());
-        tvCourseAuthor.setText(courseIntroResponse.getCourse().getInstructorName());
-        tvDescriptionContent.setText(courseIntroResponse.getCourse().getDescription());
-        tvCoursePrice.setText(String.valueOf(courseIntroResponse.getCourse().getPrice()));
-        //ratingBar.setRating(courseIntroResponse.getAverageStars().floatValue());
-        Glide.with(this).load(courseIntroResponse.getCourse().getPicture()).into(imgCourseIntro);
     }
 
     private void loadCourseLessons(String courseId) {
@@ -96,6 +66,7 @@ public class InstructorLessonActivity extends AppCompatActivity {
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(lessonListResponse -> {
+                            Log.e("kaka", lessonListResponse.getLessons().toString());
                             if (lessonListResponse != null && lessonListResponse.getLessons() != null) {
                                 lessonItemList.clear();
                                 lessonItemList.addAll(lessonListResponse.getLessons());
@@ -107,10 +78,11 @@ public class InstructorLessonActivity extends AppCompatActivity {
                             Log.e("InstructorLesson", "Error loading lessons: " + throwable.getMessage());
                         })
         );
-    }
 
+    }
     private void deleteLessonItem(int position, LessonItem lessonItem) {
-        String token = DataLocalManager.getToken();
+        String token = DataLocalManager.getToken(); // Lấy token từ local storage
+        // Xóa mục khỏi giao diện ngay lập tức
         lessonItemList.remove(position);
         lessonAdapter.notifyItemRemoved(position);
         lessonAdapter.notifyItemRangeChanged(position, lessonItemList.size());
@@ -125,16 +97,17 @@ public class InstructorLessonActivity extends AppCompatActivity {
                                         Snackbar.make(lessonsRecyclerView, "Delete successful", Snackbar.LENGTH_SHORT).show();
                                     },
                                     throwable -> {
-                                        Log.e("InstructorLesson", "Error removing item: " + throwable.getMessage());
+                                        Log.e("CartFragment", "Error removing item from cart: " + throwable.getMessage());
                                         Snackbar.make(lessonsRecyclerView, "Error removing item: " + throwable.getMessage(), Snackbar.LENGTH_SHORT).show();
                                     }
                             )
             );
         } else {
-            Log.e("InstructorLesson", "Token is null");
+            Log.e("CartFragment", "Token is null");
             Snackbar.make(lessonsRecyclerView, "Token is null", Snackbar.LENGTH_SHORT).show();
         }
     }
+
 
     @Override
     protected void onDestroy() {
